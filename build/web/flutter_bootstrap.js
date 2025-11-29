cb06ee432385f1ce83042d1b240189695d55a513
bnidigital.my <bnidigital.my@gmail.com>
2025-11-29 23:12:42 +0800

    build: Update production build with Stock Management enhancements

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3069572586"
+    serviceWorkerVersion: "2814226740"
   }
 });
